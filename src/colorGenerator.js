2f22a87228e35d1dfeb8c9a1f5a1e79712d5d987
Max Granberg <mg224em@student.lnu.se>
2023-09-21 12:32:36 +0200

    Add method to generate a random hex color string

--- src/colorGenerator.js
+++ src/colorGenerator.js
@@ -5,7 +5,7 @@ class ColorGenerator {
   /**
    * Generates a random RGB color.
    *
-   * @returns {string} generatedColor - The random generated RGB color.
+   * @returns {string} generatedColor - The random generated RGB color as a string.
    */
   generateRandomRGBColor () {
     const rColor = Math.floor(Math.random() * 256)
@@ -14,6 +14,22 @@ class ColorGenerator {
 
     const generatedColor = `rgb(${rColor},${gColor},${bColor})`
     return generatedColor
+  }
+
+  /**
+   * Generates a random HEX color.
+   *
+   * @returns {string} generatedHexColorString - The random generated HEX color as a string.
+   */
+  generateRandomHexColor () {
+    const hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    const randomHexArray = Array.from({ length: 6 })
+
+    const generatedHexColorArray = randomHexArray.map(() => hexCharacters[Math.floor(Math.random() * 16)])
+
+    const generatedHexColorString = generatedHexColorArray.join('').toString()
+
+    return `#${generatedHexColorString}`
   }
 }
 
