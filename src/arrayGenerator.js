96686dc62b3735ff8c7bcbf7f14c4491eacb271b
Max Granberg <mg224em@student.lnu.se>
2023-09-22 11:59:13 +0200

    Remove magic numbers from ArrayGenerator class

--- src/arrayGenerator.js
+++ src/arrayGenerator.js
@@ -1,35 +1,39 @@
+const DEFAULT_ARRAY_LENGTH = 10
+const MIN_VALUE_IN_ARRAY = 1
+const MAX_VALUE_IN_ARRAY = 10000
+
 /**
  * Generates a random array.
  */
 class ArrayGenerator {
+  #lengthOfArray
+
   /**
    * Creates a new ArrayGenerator instance.
    *
-   * @param {number} [lengthOfArray=10] - Desired length of array to generate.
+   * @param {number} [lengthOfArray=DEFAULT_ARRAY_LENGTH] - Desired length of array to generate.
    * If not specified the default length is set to 10.
    */
-  constructor (lengthOfArray = 10) {
-    this.lengthOfArray = lengthOfArray
+  constructor (lengthOfArray = DEFAULT_ARRAY_LENGTH) {
+    this.#validateLengthOfArray(lengthOfArray)
+    this.#lengthOfArray = lengthOfArray
   }
 
   /**
    * Generates a random array of numbers.
    *
-   * @param {number} [lengthOfArray=this.lengthOfArray] - Desired length of array to generate.
-   * If not specified the length is the default value set in the constructor
-   * @param {number} [minValueInArray=1] - Desired minimum value a index can have in the generated array.
+   * @param {number} [minValueInArray=MIN_VALUE_IN_ARRAY] - Desired minimum value a index can have in the generated array.
    * If not specified the maximum value is set to a default value of 1.
-   * @param {number} [maxValueInArray=10000] - Desired maximum value a index can have in the generated array.
+   * @param {number} [maxValueInArray=MAX_VALUE_IN_ARRAY] - Desired maximum value a index can have in the generated array.
    * If not specified the maximum value is set to a default value of 10 000.
    * @returns {Array} generatedArray - The random generated array.
    */
-  generateRandomNumbersArray (lengthOfArray = this.lengthOfArray, minValueInArray = 1, maxValueInArray = 10000) {
-    this.validateLengthOfArray(lengthOfArray)
-    this.validateNumberArray(minValueInArray, maxValueInArray)
+  generateRandomNumbersArray (minValueInArray = MIN_VALUE_IN_ARRAY, maxValueInArray = MAX_VALUE_IN_ARRAY) {
+    this.#validateNumberArray(minValueInArray, maxValueInArray)
 
     const generatedArray = []
 
-    for (let i = 0; i < lengthOfArray; i++) {
+    for (let i = 0; i < this.#lengthOfArray; i++) {
       generatedArray.push(Math.floor(Math.random() * (maxValueInArray - minValueInArray + 1) + minValueInArray)) // Push random number between minValue and maxValue
     }
 
@@ -41,7 +45,7 @@ class ArrayGenerator {
    *
    * @param {number} lengthOfArray - The custom length a user wants for their array.
    */
-  validateLengthOfArray (lengthOfArray) {
+  #validateLengthOfArray (lengthOfArray) {
     if (lengthOfArray <= 0) {
       throw new Error('lengthOfArray must be a positive number larger than 0')
     }
@@ -53,7 +57,7 @@ class ArrayGenerator {
    * @param {number} minValue - The minimum value a user wants for their array.
    * @param {number} maxValue - The maximum value a user wants for their array.
    */
-  validateNumberArray (minValue, maxValue) {
+  #validateNumberArray (minValue, maxValue) {
     if (minValue > maxValue) {
       throw new Error('minValueInArray must be less than maxValueInArray')
     }
