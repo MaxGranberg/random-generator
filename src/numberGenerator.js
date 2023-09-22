49a707d2bb07010fa333b1b685bfaa386b7d7b78
Max Granberg <mg224em@student.lnu.se>
2023-09-22 18:14:08 +0200

    Remove magic numbers from NumberGenerator class

--- src/numberGenerator.js
+++ src/numberGenerator.js
@@ -2,32 +2,22 @@
  * Generates a random number within a specified range.
  */
 class NumberGenerator {
-  /**
-   * Creates a new NumberGenerator instance.
-   *
-   * @param {number} [minValue=1] - Minimum value that the user wants for the range of numbers.
-   * @param {number} [maxValue=1000] - Maximum value that the user wants for the range of numbers.
-   * If not specified the default range is between 1 and 1000.
-   */
-  constructor (minValue = 1, maxValue = 1000) {
-    this.validateRangeInput(minValue, maxValue)
-
-    this.minValue = minValue
-    this.maxValue = maxValue
-  }
+  #defaultMinValue = 1
+  #defaultMaxValue = 10000
 
   /**
    * Generates a random number within a specified range.
    *
-   * @param {number} [minValue=1] - Minimum value that the user wants for the range of numbers.
-   * @param {number} [maxValue=1000] - Maximum value that the user wants for the range of numbers.
-   * If not specified the range is the default values set in the constructor.
+   * @param {number} [minValue=this.#defaultMinValue] - Minimum value that the user wants for the range of numbers.
+   * If not specified the range is the default value 1.
+   * @param {number} [maxValue=this.#defaultMaxValue] - Maximum value that the user wants for the range of numbers.
+   * If not specified the range is the default value 10000.
    * @returns {number} randomNumber - A randomly generated number.
    */
-  generateRandomNumber (minValue = this.minValue, maxValue = this.maxValue) {
-    this.validateRangeInput(minValue, maxValue)
+  generateRandomNumber (minValue = this.#defaultMinValue, maxValue = this.#defaultMaxValue) {
+    this.#validateRangeInput(minValue, maxValue)
 
-    const randomNumber = Math.floor(Math.random() * (maxValue - minValue) + minValue)
+    const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue)
 
     return randomNumber
   }
@@ -38,7 +28,7 @@ class NumberGenerator {
    * @param {number} minValue - Minimum value that the user wants for the range of numbers.
    * @param {number} maxValue - Maximum value that the user wants for the range of numbers.
    */
-  validateRangeInput (minValue, maxValue) {
+  #validateRangeInput (minValue, maxValue) {
     if (typeof minValue !== 'number' || typeof maxValue !== 'number') {
       throw new Error('Values must be of type number')
     }
