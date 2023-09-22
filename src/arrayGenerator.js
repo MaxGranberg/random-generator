a61e03609d0d88ba2340fb23cd7c5e6e5f13ca31
Max Granberg <mg224em@student.lnu.se>
2023-09-22 11:28:37 +0200

    Create an ArrayGenerator class

---
+++ src/arrayGenerator.js
@@ -0,0 +1,37 @@
+/**
+ * Generates a random array.
+ */
+class ArrayGenerator {
+  /**
+   * Creates a new ArrayGenerator instance.
+   *
+   * @param {number} [lengthOfArray=10] - Desired length of array to generate.
+   * If not specified the default length is set to 10.
+   */
+  constructor (lengthOfArray = 10) {
+    this.lengthOfArray = lengthOfArray
+  }
+
+  /**
+   * Generates a random array of numbers.
+   *
+   * @param {number} [lengthOfArray=this.lengthOfArray] - Desired length of array to generate.
+   * If not specified the length is the default value set in the constructor
+   * @param {number} [minValueInArray=1] - Desired minimum value a index can have in the generated array.
+   * If not specified the maximum value is set to a default value of 1.
+   * @param {number} [maxValueInArray=10000] - Desired maximum value a index can have in the generated array.
+   * If not specified the maximum value is set to a default value of 10 000.
+   * @returns {Array} generatedArray - The random generated array.
+   */
+  generateRandomNumbersArray (lengthOfArray = this.lengthOfArray, minValueInArray = 1, maxValueInArray = 10000) {
+    const generatedArray = []
+
+    for (let i = 0; i < lengthOfArray; i++) {
+      generatedArray.push(Math.floor(Math.random() * (maxValueInArray - minValueInArray + 1) + minValueInArray)) // Push random number between minValue and maxValue
+    }
+
+    return generatedArray
+  }
+}
+
+export default ArrayGenerator
