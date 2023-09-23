3fc12a50e3474769e4eef4db748dc78b1f0295cd
Max Granberg <mg224em@student.lnu.se>
2023-09-23 09:21:01 +0200

    Remove magic numbers from ColorGenerator class

--- src/colorGenerator.js
+++ src/colorGenerator.js
@@ -2,17 +2,18 @@
  * Generates a random color.
  */
 class ColorGenerator {
+  #rgbMaxValue = 256
   /**
    * Generates a random RGB color.
    *
    * @returns {string} generatedColor - The random generated RGB color as a string.
    */
   generateRandomRGBColor () {
-    const rColor = Math.floor(Math.random() * 256)
-    const gColor = Math.floor(Math.random() * 256)
-    const bColor = Math.floor(Math.random() * 256)
+    const redValue = Math.floor(Math.random() * this.#rgbMaxValue)
+    const greenValue = Math.floor(Math.random() * this.#rgbMaxValue)
+    const blueValue = Math.floor(Math.random() * this.#rgbMaxValue)
 
-    const generatedColor = `rgb(${rColor},${gColor},${bColor})`
+    const generatedColor = `rgb(${redValue},${greenValue},${blueValue})`
     return generatedColor
   }
 
@@ -25,7 +26,7 @@ class ColorGenerator {
     const hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     const randomHexArray = Array.from({ length: 6 })
 
-    const generatedHexColorArray = randomHexArray.map(() => hexCharacters[Math.floor(Math.random() * 16)])
+    const generatedHexColorArray = randomHexArray.map(() => hexCharacters[Math.floor(Math.random() * hexCharacters.length)])
 
     const generatedHexColorString = generatedHexColorArray.join('').toString()
 
