909f7ac79ddb2ced4bfc0cdd64f2dd2661f426a9
Max Granberg <mg224em@student.lnu.se>
2023-09-24 13:45:44 +0200

    Update index.js with classes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import StringGenerator from './stringGenerator.js'
+import NumberGenerator from './numberGenerator.js'
+import DateGenerator from './dateGenerator.js'
+import ColorGenerator from './colorGenerator.js'
+import ArrayGenerator from './arrayGenerator.js'
+
+export {
+  StringGenerator,
+  NumberGenerator,
+  DateGenerator,
+  ColorGenerator,
+  ArrayGenerator
+}
