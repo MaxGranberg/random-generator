2231cd7e0a7579fcc4202305c8de6ed32c46397e
Max Granberg <mg224em@student.lnu.se>
2023-09-25 09:58:47 +0200

    Add automated tests for ColorGenerator

--- tests/colorGenerator.test.js
+++ tests/colorGenerator.test.js
@@ -1,10 +1,30 @@
 import ColorGenerator from '../src/colorGenerator.js'
 
 describe('ColorGenerator', () => {
-  const colorGen = new ColorGenerator()
+  let colorGenerator
 
-  test('should generate a valid HEX color string', () => {
-    const result = colorGen.generateRandomHexColor()
-    expect(result).toMatch(/^#[0-9A-F]{6}$/)
+  beforeEach(() => {
+    colorGenerator = new ColorGenerator()
+  })
+
+  describe('generateRandomRGBColor', () => {
+    it('should generate a valid RGB color string', () => {
+      const result = colorGenerator.generateRandomRGBColor()
+      expect(result).toMatch(/^rgb\(\d{1,3},\d{1,3},\d{1,3}\)$/)
+
+      // Extract numbers from string and check if they are within the range 0-255
+      const numbers = result.match(/\d+/g).map(Number)
+      numbers.forEach((number) => {
+        expect(number).toBeGreaterThanOrEqual(0)
+        expect(number).toBeLessThan(256)
+      })
+    })
+  })
+
+  describe('generateRandomHexColor', () => {
+    it('should generate a valid HEX color string', () => {
+      const result = colorGenerator.generateRandomHexColor()
+      expect(result).toMatch(/^#[0-9A-F]{6}$/)
+    })
   })
 })
